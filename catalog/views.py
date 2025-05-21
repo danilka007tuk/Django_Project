c8adb42cde38e6044eb4e56adf94df630368895f
Данил <68993829+danilka007tuk@users.noreply.github.com>
2025-05-21 21:24:55 +0500

    Commit doc create home.html

--- catalog/views.py
+++ catalog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 #Создание контроллеров на приложение catalog
 
 def sfera(request):
-    return render(request, 'catalog', name='catalog')
+    return render(request, 'catalog/home.html')
